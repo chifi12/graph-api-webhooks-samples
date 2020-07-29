ebfff5318f61d78b5f83e9957e1a69e2091a5570
Esteban Ferreyra <esteban128@gmail.com>
2020-07-29 20:33:41 -0300

    prueba 3

--- heroku/index.js
+++ heroku/index.js
@@ -22,7 +22,7 @@ var received_updates = [];
 
 app.get('/', function(req, res) {
   console.log(req);
-  res.send('<pre>' + JSON.stringify(received_updates, res.body, 2) + '</pre>');
+  res.send('<pre>' + JSON.stringify(received_updates, "hola" , 2) + '</pre>');
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
@@ -38,9 +38,9 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 
 app.post('/instagram', function(req, res) {
   console.log('Instagram request body:');
-  console.log(req);
+  console.log(req.body);
   // Process the Instagram updates here
-  received_updates.unshift(req);
+  received_updates.unshift(req.body);
   res.sendStatus(200);
 });
 
