51cad2cfb3d9bc42f6ef4588be8a5e516f9dfe98
FedericoChediack <fefechediack@gmail.com>
2020-08-05 19:33:10 -0300

    esto es un manotazo de ahogado

--- heroku/index.js
+++ heroku/index.js
@@ -1,50 +1,50 @@
-/**
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// /**
+//  * Copyright 2016-present, Facebook, Inc.
+//  * All rights reserved.
+//  *
+//  * This source code is licensed under the license found in the
+//  * LICENSE file in the root directory of this source tree.
+//  */
 
-var bodyParser = require('body-parser');
-var express = require('express');
-var app = express();
-var xhub = require('express-x-hub');
+// var bodyParser = require('body-parser');
+// var express = require('express');
+// var app = express();
+// var xhub = require('express-x-hub');
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'));
-process.env.APP_SECRET = "";
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
-app.use(bodyParser.json());
+// app.set('port', (process.env.PORT || 5000));
+// app.listen(app.get('port'));
+// process.env.APP_SECRET = "";
+// app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+// app.use(bodyParser.json());
 
-var token = process.env.TOKEN || 'token';
-var received_updates = [];
+// var token = process.env.TOKEN || 'token';
+// var received_updates = [];
 
-app.get('/', function(req, res) {
-  console.log(req.body);
-  console.log(req);
-  console.log(JSON.stringify(received_updates, req.body));
-  res.send('<pre>' + JSON.stringify(received_updates, req , 2) + '</pre>');
-});
+// app.get('/', function(req, res) {
+//   console.log(req.body);
+//   console.log(req);
+//   console.log(JSON.stringify(received_updates, req.body));
+//   res.send('<pre>' + JSON.stringify(received_updates, req , 2) + '</pre>');
+// });
 
-app.get(['/facebook', '/instagram'], function(req, res) {
-  if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == token
-  ) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(400);
-  }
-});
+// app.get(['/facebook', '/instagram'], function(req, res) {
+//   if (
+//     req.query['hub.mode'] == 'subscribe' &&
+//     req.query['hub.verify_token'] == token
+//   ) {
+//     res.send(req.query['hub.challenge']);
+//   } else {
+//     res.sendStatus(400);
+//   }
+// });
 
-app.post('/instagram', function(req, res) {
-  console.log('Instagram request body:');
-  console.log(req.body);
+// app.post('/instagram', function(req, res) {
+//   console.log('Instagram request body:');
+//   console.log(req.body);
   
-  // Process the Instagram updates here
-  received_updates.unshift(req.body);
-  res.send('<pre>' + JSON.stringify(received_updates, req , 2) + '</pre>');
-});
+//   // Process the Instagram updates here
+//   received_updates.unshift(req.body);
+//   res.send('<pre>' + JSON.stringify(received_updates, req , 2) + '</pre>');
+// });
 
-app.listen();
+// app.listen();
